72858adb5d9377d8e2c6044667cd7738a31f73bd
Luna Lolita <wolfitdm@gmail.com>
2026-02-16 16:43:20 +0100

    add more

--- obj/Release/net40/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BitchlandMoreWeaponSlotsBepInEx
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BitchlandMoreWeaponSlotsBepInEx";
-        public const string PLUGIN_NAME = "You have unlimited all!";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
